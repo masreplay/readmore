2153ba4f4c1a1eb485745aa68d2b5c68d246f37a
M Atheer <55885230+masreplay@users.noreply.github.com>
2023-11-18 19:33:22 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,39 +1,31 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+# readmore_plus
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
+A Flutter plugin that allows to expansion and collapse of text.
 
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
+# Support flutter 3
+![](read-more-text-view-flutter.gif)
 
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+## usage:
+add to your pubspec
 
-## Features
-
-TODO: List what your package can do. Maybe include images, gifs, or videos.
-
-## Getting started
-
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
-
-## Usage
-
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder.
-
-```dart
-const like = 'sample';
+```
+readmore: 
+```
+and import:
+```
+import 'package:readmore/readmore.dart';
 ```
 
-## Additional information
+```dart
+ReadMoreText(
+  'Flutter is Google’s mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase.',
+  trimLines: 2,
+  colorClickableText: Colors.pink,
+  trimMode: TrimMode.line,
+  trimCollapsedText: 'Show more',
+  trimExpandedText: 'Show less',
+  moreStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+);
+```
 
-TODO: Tell users more about the package: where to find more information, how to
-contribute to the package, how to file issues, what response they can expect
-from the package authors, and more.
+
---
+++ example/lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:readmore_plus/readmore_plus.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Read More Text',
+      home: DemoApp(),
+    );
+  }
+}
+
+class DemoApp extends StatelessWidget {
+  const DemoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Read More Text',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: DefaultTextStyle.merge(
+        style: const TextStyle(
+          fontSize: 16.0,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Padding(
+                key: Key('showMore'),
+                padding: EdgeInsets.all(16.0),
+                child: ReadMoreText(
+                  'Flutter is Google’s mobile UI open source framework to build high-quality native (super fast) interfaces for iOS and Android apps with the unified codebase.',
+                  trimLines: 2,
+                  preDataText: "AMANDA",
+                  preDataTextStyle: TextStyle(fontWeight: FontWeight.w500),
+                  style: TextStyle(color: Colors.black),
+                  colorClickableText: Colors.pink,
+                  trimMode: TrimMode.line,
+                  trimCollapsedText: '...Show more',
+                  trimExpandedText: ' show less',
+                ),
+              ),
+              const Divider(
+                color: Color(0xFF167F67),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ReadMoreText(
+                  'Flutter(https://flutter.dev/) has its own UI components, along with an engine to render them on both the Android and iOS platforms. Most of those UI components, right out of the box, conform to the guidelines of Material Design.',
+                  trimLines: 3,
+                  style: const TextStyle(color: Colors.black),
+                  colorClickableText: Colors.pink,
+                  trimMode: TrimMode.line,
+                  trimCollapsedText: '...Expand',
+                  trimExpandedText: ' Collapse ',
+                  onLinkPressed: (url) {
+                    debugPrint(url);
+                  },
+                ),
+              ),
+              const Divider(
+                color: Color(0xFF167F67),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: ReadMoreText(
+                  'The Flutter framework builds its layout via the composition of widgets, everything that you construct programmatically is a widget and these are compiled together to create the user interface. ',
+                  trimLines: 2,
+                  style: TextStyle(color: Colors.black),
+                  colorClickableText: Colors.pink,
+                  trimMode: TrimMode.line,
+                  trimCollapsedText: '...Read more',
+                  trimExpandedText: ' Less',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
